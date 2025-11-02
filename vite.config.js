b35d24f3e757ae6f0d0ec1bddc38bedfaf2aba00
nurhanzamalbekov-bit <nurhanzamalbekov@gmail.com>
2025-11-02 23:14:37 +0300

    new-test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Adding-my-first-React-attempt-to-GitHub-following-a-tutorial'
 })
